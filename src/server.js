4569bb9bf1c3700de0f249d8c25d5c381fde2051
ravi <artiveloper@gmail.com>
2020-08-28 13:06:55 +0900

    fix: graphql 과 resolver 폴더를 구분하여 설정한다

--- src/server.js
+++ src/server.js
@@ -1,24 +1,25 @@
 import dotenv from 'dotenv'
 import logger from 'morgan'
+import path from 'path'
 import {GraphQLServer} from 'graphql-yoga'
+import {makeExecutableSchema} from 'graphql-tools'
+import {fileLoader, mergeTypes, mergeResolvers} from 'merge-graphql-schemas'
 
 dotenv.config()
 
 const PORT = process.env.PORT || 4000
 
-const typeDefs = `
-    type Query {
-        hello: String!
-    }
-`
+/*
+    graphql configuration.
+ */
+const allTypes = fileLoader(path.join(__dirname, '/graphqls/*'))
+const allResolvers = fileLoader(path.join(__dirname, '/resolvers/*'))
+const schema = makeExecutableSchema({
+    typeDefs: mergeTypes(allTypes),
+    resolvers: mergeResolvers(allResolvers)
+})
 
-const resolvers = {
-    Query: {
-        hello: () => "Hi"
-    }
-}
-
-const server = new GraphQLServer({typeDefs, resolvers})
+const server = new GraphQLServer({schema})
 server.express.use(logger("dev"))
 
 server.start({port: PORT}, () => {
